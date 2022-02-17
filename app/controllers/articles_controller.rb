c247605146fc9b720771a6a137e71b93812c0b48
Javier Garcia <jgarcia@lwolf.com>
2022-02-16 19:48:43 -0500

    add user controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,26 +4,31 @@ class ArticlesController < ApplicationController
   end
   def show
     @article = Article.find(params[:id])
+    @user = User.find(@article.user_id)
   end
   def new
     @article = Article.new
+    @users = User.all
   end
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
+      @users = User.all
       render :new, status: :unprocessable_entity
     end
   end
   def edit
     @article = Article.find(params[:id])
+    @users = User.all
   end
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
+      @users = User.all
       render :edit, status: :unprocessable_entity
     end
   end
@@ -35,6 +40,6 @@ class ArticlesController < ApplicationController
   end
   private
   def article_params
-    params.require(:article).permit(:title, :body, :status)
+    params.require(:article).permit(:title, :body, :status, :user_id)
   end
 end

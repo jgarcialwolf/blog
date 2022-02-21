6d30645e3037c3bd310fae09334fcd7bb8a456d0
Javier Garcia <jgarcia@lwolf.com>
2022-02-21 10:15:25 -0500

    add feature for filtering articles for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    id = params[:id]
+    @user = User.find(id)
+    @articles = Article.where(user_id: id)
+  end
 end
